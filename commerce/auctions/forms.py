d5f635845608033a0a365f78c0b1855c089a6d96
[eld120] <devteam@origma.io>
2021-03-23 12:40:38 -0500

    ready to develop

---
+++ commerce/auctions/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.forms.models import ModelForm
+from django.views.generic.edit import CreateView
+from .models import Listing, Bid, Comment
+
+
+class ListingCreateForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = [ 'title','image', 'description', 'active', 'start_price']
+   
+
+# class BidForm(ModelForm):
+#     class Meta:
+#         model = Bid
+#         fields = ['listing_id', 'listing_bid']
+
+# class CommentForm(ModelForm):
+#     class Meta:
+#         model = Comment
+#         fields = ['listing_id', 'text']
--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auctions'
 ]
 
 MIDDLEWARE = [
@@ -80,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'auctions.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR.joinpath('static'))
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
