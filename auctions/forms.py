217650a128776152dc103cffd03e1b603dce36ac
eld120 <devteam@origma.io>
2022-01-07 20:34:49 -0600

    more listing detail css changes

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.db.models.base import Model
 from django.db.models.fields import BooleanField
 from django.forms.models import ModelForm
@@ -17,7 +18,13 @@ class BidForm(ModelForm):
     class Meta:
         model = Bid
         fields = ["bid"]
-
+    
+    def clean(self):
+        if self.bid < 0:
+            raise forms.ValidationError('Your bid must be a positive value', code="confirm_positive_bid")
+        
+        #TODO - validate that the bid is larger than any other previous bids
+        return self.cleaned_data
 
 class EndForm(forms.Form):
     active = forms.BooleanField(required=False)
