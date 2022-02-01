aa530675cfeb29566d78525f162ecd15b8a87545
eld120 <devteam@origma.io>
2022-02-01 16:36:18 -0600

    workaround Faker bug: https://github.com/joke2k/faker/issues/1538

--- auctions/tests/factories.py
+++ auctions/tests/factories.py
@@ -1,8 +1,9 @@
 import datetime
 
 import factory
-from django.conf import settings
 from django.utils import text, timezone
+
+from commerce.settings import AUTH_USER_MODEL
 
 """
 Attributing inspiration/guidance for the organization of this pytest implementation to
@@ -14,11 +15,11 @@ https://github.com/mblayman/homeschool/blob/main/homeschool/users/tests/factorie
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = settings.AUTH_USER_MODEL
+        model = AUTH_USER_MODEL
 
     username = factory.Sequence(lambda x: f"rando_user_{x}")
     password = factory.PostGenerationMethodCall("set_password", "password")
-    cash = factory.Faker("pyint", min_value=1999, max_value=99999)
+    cash = factory.Faker("pyint", min_value=199999, max_value=999999) / 100
 
 
 class ListingFactory(factory.django.DjangoModelFactory):
@@ -28,9 +29,9 @@ class ListingFactory(factory.django.DjangoModelFactory):
     title = factory.Faker("text", max_nb_chars=20)
     slug = text.slugify(title)
     description = factory.Faker("sentence", nb_words=30)
-    image = factory.django.ImageField
+    image = factory.Faker("file_path", depth=3)
     active = True  # should change if we ever do something with the active flag
-    start_price = factory.Faker("pyfloat", min_value=0.25, max_value=1199.99)
+    start_price = factory.Faker("pyint", min_value=25, max_value=119999) / 100
     auction_start = factory.LazyFunction(lambda: timezone.localdate())
     auction_end = factory.LazyFunction(
         lambda: timezone.localdate() + datetime.timedelta(days=7)
@@ -51,7 +52,7 @@ class BidFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = "auctions.Bid"
 
-    bid = factory.Faker("pyfloat", min_value=1.99, max_value=1199.99)
+    bid = factory.Faker("pyint", positive=True, max_value=1199.99) / 100
     date = factory.LazyFunction(lambda: timezone.localdate())
     winning_bid = False  # currently not implemented
     owner = factory.SubFactory(UserFactory)
